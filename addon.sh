e31fd7a0f30e42c60424b829b56a4f9e22675332
SARTAMP <139582244+SARTAMP@users.noreply.github.com>
2023-11-07 22:23:11 +0700

    Update addon.sh

--- addon.sh
+++ addon.sh
@@ -3,7 +3,7 @@ cd /root
 curl -L https://indo-ssh.com/backup.sh >> backup.sh
 cp backup.sh /usr/bin/backup
 chmod +x /usr/bin/backup
-#echo "0 0,6,12,18 * * * root backup" >> /etc/crontab
+echo "0 0,6,12,18 * * * root backup" >> /etc/crontab
 systemctl restart cron
 service cron restart
 rm -rf addon.sh
